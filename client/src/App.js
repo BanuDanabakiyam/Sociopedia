76d45d9845a232c8efa9aba1c932c754aee3a79b
BanuDanabakiyam <banubala9655@gmail.com>
2023-06-28 10:58:38 +0530

    moreInRegister

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from 'scenes/loginPage/index.jsx';
 import HomePage from "scenes/homePage/index.jsx";
