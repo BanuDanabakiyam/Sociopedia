37fe0648b7d806e615ef171e02066309d088b35b
BanuDanabakiyam <banubala9655@gmail.com>
2023-06-29 11:44:30 +0530

    Final code

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,44 +13,43 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-        `http://localhost:3001/users/${userId}/friends`,
-        {
-            method: "GET",
-            headers: { Authorization: `Bearer ${token}`},
-
-        }
+      `http://localhost:3001/users/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
     );
     const data = await response.json();
     dispatch(setFriends({ friends: data }));
   };
+
   useEffect(() => {
     getFriends();
-  },[]);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <WidgetWrapper>
-        <Typography
+      <Typography
         color={palette.neutral.dark}
         variant="h5"
         fontWeight="500"
-        sx={{ mb: "1.5rem"}}
-        >
-
-            Friend List
-        </Typography>
-        <Box display="flex" flexDirection="column" gap="1.5rem">
-            {friends.map((friend) => (
-                <Friend
-                key={friend._id}
-                friendId={friend._id}
-                name={`${friend.firstName} ${friend.lastName}`}
-                subtitle={friend.occupation}
-                userPicturePath={friend.picturePath}
-                ></Friend>
-            ))}
-        </Box>
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Box display="flex" flexDirection="column" gap="1.5rem">
+        {friends.map((friend) => (
+          <Friend
+            key={friend._id}
+            friendId={friend._id}
+            name={`${friend.firstName} ${friend.lastName}`}
+            subtitle={friend.occupation}
+            userPicturePath={friend.picturePath}
+          />
+        ))}
+      </Box>
     </WidgetWrapper>
   );
+};
 
-}
- export default FriendListWidget;
+export default FriendListWidget;
